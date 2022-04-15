7b515a325d2c776a36bf2d1b19060610aac07f90
thestriver <wannabeamumin@gmail.com>
2022-04-15 23:52:00 +0100

    hero clr

--- pages/hero.js
+++ pages/hero.js
@@ -3,10 +3,6 @@ import Image from 'next/image'
 const Hero = () => {
     return(
         <div>
-            {/* ====== Hero Section Start */}
-            <div className="relative pt-[60px] lg:pt-[50px] pb-[50px] bg-white">        
-            </div>
-            {/* Hero Section End */}
         </div>
     )
 }
