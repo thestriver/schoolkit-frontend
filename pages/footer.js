56c6e02dd8dafad0a932486b827effd30c428e91
thestriver <wannabeamumin@gmail.com>
2022-04-19 19:25:00 +0100

    comp struct

--- pages/footer.js
+++ pages/footer.js
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 const Footer = () => {
     return(
-        <footer className="text-gray-900 bg-gray-200 body-font cursor-pointer">
-            <div className="lg:container px-5 py-2 lg:mx-auto flex lg:items-center sm:flex-row flex-col">
+        <footer className="text-center xl:px-12 bg-gray-900 text-gray-200 body-font align-center cursor-pointer border-t border-gray-200 border-t-1">
+            <div className="lg:container px-5 py-2 lg:mx-auto flex  sm:flex-row flex-col">
+            <a href="https://twitter.com/The_striver" className="text-sm text-gray-500  sm:py-2 sm:mt-0 mt-4">Created with <span className="px-0">❤️</span> by Mahmud Adeleye
+            </a>
             </div>
         </footer>
 
