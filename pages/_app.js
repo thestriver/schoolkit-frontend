ad8d328e023d0b742bb95d4298dee00426dcc7e0
thestriver <wannabeamumin@gmail.com>
2022-04-20 21:52:00 +0100

    solan a imp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,37 @@
+import '../styles/globals.css'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { clusterApiUrl } from '@solana/web3.js'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  // const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = clusterApiUrl(network);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = [
+    new PhantomWalletAdapter(),
+  ];
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+            <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
 
 export default MyApp
