a64e329a4e2e1440ec4813b8394c18d4cd685421
thestriver <wannabeamumin@gmail.com>
2022-04-22 08:15:00 +0100

    donors set

--- pages/givers.js
+++ pages/givers.js
@@ -7,7 +7,25 @@ const Givers = () => {
                 Hearty thanks to these companies and individuals for supporting our cause
             </h1>
             <div className="grid grid-cols-2 gap-10 text-center lg:grid-cols-8">
+            <div className="flex items-center justify-center gap-x-5">
+                {
+                   donors.map(({ ...props}) => (
+                        <div key={props.link}>
+                        <a href={props.link} target="_blank" className="flex flex-col items-center text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600" rel="noreferrer">
+                        <Image
+                            src={props.pic}
+                            alt="donor"
+                            width={72}
+                            height={72}
+                            className="h-12 w-72 rounded-md"
 
+                        />
+                        </a>
+                         <small>{props?.donor}</small>
+                        </div>
+                    ))
+                }
+                </div>
             </div>
         </section>
     )
